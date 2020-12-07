be28a562b929335b663bf2b8f24aef8ab78cc263
Jerome Twell <jtwell1@gmail.com>
2020-12-07 21:59:54 +0000

    add meatballs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Head from 'next/head'
 import styles from './index.module.css'
+import useSound from 'use-sound';
+import {useState} from "react"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faInstagram  } from '@fortawesome/free-brands-svg-icons'
 
+
 export default function Home() {
+  const [play, {stop}] = useSound("/meatball.mp3");
+  const handleClick = () => {
+    stop();
+    play();
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +22,7 @@ export default function Home() {
       <div className={styles.cover}></div>
       <main className={styles.main}>
         <h1 className={styles.title}>Coming Soon</h1>
-        <img src="/stickerpeel.png" width={350} height={350} className={styles.logo} />
+        <img onClick={handleClick} src="/stickerpeel.png" width={350} height={350} className={styles.logo} />
         
         <div className={styles.brandsBar}>
           <a href="https://www.facebook.com/JTsDangerKetchup"><FontAwesomeIcon icon={faFacebook} className={styles.brandIcon} color="white" /></a>
